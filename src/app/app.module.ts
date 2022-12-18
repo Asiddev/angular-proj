f68be7b7e49d0c98e4f8ce3d2c7261c62288ee8a
Alexander Sidorenko <asiddev@protonmail.com>
2022-12-17 22:30:40 -0800

    add userName double-data-binding on input , and use conditionals for button property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SuccessComponent } from './success/success.component';
 import { WarningComponent } from './warning/warning.component';
+import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, SuccessComponent, WarningComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    SuccessComponent,
+    WarningComponent,
+    ServersComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
