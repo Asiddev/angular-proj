9b727217107f03ac3941bd03ffc3e216f5de74f3
Alexander Sidorenko <asiddev@protonmail.com>
2022-12-18 22:51:53 -0800

    practice button click event with ngClass for dynamic styling

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   username = '';
   allowSubmit = true;
+  clicked = false;
 
   constructor() {}
 
